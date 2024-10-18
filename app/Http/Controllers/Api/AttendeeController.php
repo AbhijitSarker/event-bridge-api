a1cd7e0079711007da2ef7b08768eb39482bc526
AbhijitSarker <abhijitsarker03@gmail.com>
2024-10-18 20:04:40 +0600

    loaded attendee relations

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -7,15 +7,22 @@ use Illuminate\Http\Request;
 use App\Http\Resources\AttendeeResource;
 use App\Models\Attendee;
 use App\Models\Event;
+use App\Http\Traits\CanLoadRelationships;
 
 class AttendeeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
+    use CanLoadRelationships;
+    private array $relations = ['user'];
+
     public function index(Event $event)
     {
-        $attendees = $event->attendees()->latest();
+        $attendees = $this->loadRelationships(
+            $event->attendees()->latest()
+        );
 
         return AttendeeResource::collection(
             $attendees->paginate()
@@ -27,9 +34,12 @@ class AttendeeController extends Controller
      */
     public function store(Request $request, Event $event)
     {
-        $attendee = $event->attendees()->create([
-            'user_id' => 2
-        ]);
+        $attendee = $this->loadRelationships(
+            $event->attendees()->create([
+                'user_id' => 1
+            ])
+        );
+
         return new AttendeeResource($attendee);
     }
 
@@ -38,8 +48,10 @@ class AttendeeController extends Controller
      */
     public function show(Event $event, Attendee $attendee)
     {
-        return new AttendeeResource($attendee);
-    }
+        return new AttendeeResource(
+            $this->loadRelationships($attendee)
+        );   
+     }
 
     /**
      * Update the specified resource in storage.
--- app/Http/Traits/CanLoadRelationships.php
+++ app/Http/Traits/CanLoadRelationships.php
@@ -5,14 +5,17 @@ namespace App\Http\Traits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait CanLoadRelationships
 {
   public function loadRelationships(
-    Model|QueryBuilder|EloquentBuilder $for,
+    Model|QueryBuilder|EloquentBuilder|HasMany $for,
+
     ?array $relations = null
-  ): Model|QueryBuilder|EloquentBuilder 
-  {
+    
+    ): Model|QueryBuilder|EloquentBuilder|HasMany {
+
     $relations = $relations ?? $this->relations ?? [];
 
     foreach ($relations as $relation) {
@@ -28,10 +31,13 @@ trait CanLoadRelationships
   protected function shouldIncludeRelation(string $relation): bool
   {
     $include = request()->query('include');
+
     if (!$include) {
       return false;
     }
+
     $relations = array_map('trim', explode(',', $include));
+
     return in_array($relation, $relations);
   }
 }
