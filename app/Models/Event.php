4ce6ebfc43a32e2e3fdbf28f7937dddf33568893
AbhijitSarker <abhijitsarker03@gmail.com>
2024-10-18 16:36:35 +0600

    adde create event route

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -11,10 +11,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'start_time', 'end_time', 'user_id'];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
+    
     public function attendees(): HasMany
     {
         return $this->hasMany(Attendee::class);
